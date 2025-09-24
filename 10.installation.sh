fc87d6181fc0fd383ec99f2a92000a86f4f9c916
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-24 23:16:27 +0530

    installation-3

--- 10.installation.sh
+++ 10.installation.sh
@@ -5,3 +5,12 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Error :: please run the script by using root user"
 fi
+
+
+dnf insatll mysql -y
+
+if [ $? -ne 0 ]; then
+     echo "Error :: failed to install mysql"
+else
+    echo "installing the mysql server"
+fi
