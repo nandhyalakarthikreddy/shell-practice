b11b92d17092d42529107c87b2587db0b49b2af7
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-24 22:48:19 +0530

    conditions-3

--- 8.conditions.sh
+++ 8.conditions.sh
@@ -2,9 +2,9 @@
 
 number=$1
 
-if[ $number -lt 10 ]; then
+if [ $number -lt 10 ]; then
     echo "given number is $number less than 10"
-elif[ $number -eq 10 ]; then
+elif [ $number -eq 10 ]; then
     echo "given number is $number equal to 10"
 else
     echo "given number is $number greater than 10"
