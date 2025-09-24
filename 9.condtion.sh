c7c767128467c550945cd0a0b90ab3c7303ab306
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-24 22:53:57 +0530

    conditions-1

---
+++ 9.condtion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter the number"
+read NUMBER
+
+if [ $(($NUMBER % 2)) ]; then
+    echo "given number is $NUMBER even number"
+else
+    echo "given number is $NUMBER odd number"
