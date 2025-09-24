a73f535ce64854eb1f6a047a654ed779af68d4df
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-24 22:23:44 +0530

    data-types-4

--- 7.datatypes.sh
+++ 7.datatypes.sh
@@ -2,5 +2,9 @@
 
 number1=100
 number2=200
-sum=$(($number1+$number2))
+name=devops
+sum=$(($number1+$number2+$name))
 echo "the total sum is: ${sum}"
+
+list=("linux" "karthik" "python" "devops")
+echo "all leaders:${list[@]}"
