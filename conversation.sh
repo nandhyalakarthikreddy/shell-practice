a462c043fd997d2102e8063270e186e8beeaec2b
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-24 19:12:51 +0530

    1st round

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "karthik :: hi ramu "
-echo "ramu :: hello karthik hru"
-echo "karthik :: tell me about your self"
-echo "ramu :: my self nothing"
+Name1=karthik
+Name2=likith
+echo "${Name1}:: hi ${Name2}"
+echo "${Name2}:: hello ${Name1} hru"
+echo "${Name1}:: tell me about your self"
+echo "${Name2}:: my self nothing"
