5058a586163d6e9583c728fd968d3a21e1dac66e
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-13 17:24:49 +0530

    install-example

--- 17.example.sh
+++ 17.example.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "Error :: please run the script by using root user"
+    echo -e " $R Error :: please run the script by using root user $N "
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "Error :: Failed to install $2 server"
+        echo -e "$R Error :: Failed to install $2 server $N"
+        exit 1
     else
-        echo "installing $2 server"
+        echo "$G installing $2 server $N"
     fi
 }
 
