6d952707d955b64971dbbafcbf3c335f26cecdeb
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-13 16:45:18 +0530

    install-example

--- 17.example.sh
+++ 17.example.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]; then
-    echo "Failed to install try using root user"
-else 
-    echo "successfully installed "
+    echo "Error :: please run the script by using root user"
+    exit 1
 fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "Fialed to install "
+else 
+    echo "successfully installed"
